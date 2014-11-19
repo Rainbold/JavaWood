853fe4a5d35503f19bbd57833af332859aeb3abf
RainbowLyte <rainbowlyte42@gmail.com>
2014-11-19 11:56:00 +0100

    [XML] Parser for "Fournisseurs"

--- src/gestion/Simulation.java
+++ src/gestion/Simulation.java
@@ -1,17 +1,29 @@
 package gestion;
 
+import gestion.algorithme.Algorithme;
 import gestion.bois.*;
+
+import java.io.FileNotFoundException;
 import java.util.*;
+
+import javax.xml.stream.XMLStreamException;
 
 public class Simulation {
 
 	public static void main(String[] args) {
 
-		Set<Planche> planches = new HashSet<Planche>();
-		Set<Commande> commandes = new HashSet<Commande>();
+		HashSet<Planche> planches = new HashSet<Planche>();
+		HashSet<Commande> commandes = new HashSet<Commande>();
 		
-		
-		
+		try {
+			Algorithme.XMLParseFournisseur("fournisseurs.xml", planches);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		System.out.println("Ferme la !");
 	}
--- src/gestion/algorithme/Algorithme.java
+++ src/gestion/algorithme/Algorithme.java
@@ -1,11 +1,65 @@
 package gestion.algorithme;
 
+import  gestion.bois.*;
+import javax.xml.stream.*;
+import java.io.*;
+import java.util.HashSet;
+import javax.xml.stream.events.XMLEvent;
+
 public abstract class Algorithme {
 	
-	public void serialisation() {
-		
-		
-		
+
+	public String Methode1() {
+		return "";
 	}
-	
+
+	public String Methode2() {
+		return "";
+	}
+
+	public static String XMLParseFournisseur(String file, HashSet<Planche> planches) throws FileNotFoundException, XMLStreamException {
+		XMLInputFactory f = XMLInputFactory.newInstance();
+		FileInputStream fis = new FileInputStream(file);
+	    XMLStreamReader r = f.createXMLStreamReader(fis);
+	   
+	    int i = 0;
+	    
+		while(r.hasNext()) {
+			int eventType = r.next();
+            switch(eventType) {
+                case XMLEvent.START_ELEMENT:
+                	if(i == 0 && r.getLocalName() != "fournisseurs")
+            		{
+                		// Todo throw an exception
+            			System.err.println("Fichier " + file + " invalide");
+            			System.exit(1);
+            		}
+                	
+                	switch(r.getLocalName())
+                	{
+                		case "fournisseur":
+                			int attributes = r.getAttributeCount();
+                			for(int index=0; index<attributes; index++) {
+                    			System.out.println(r.getAttributeLocalName(index) + " : " + r.getAttributeValue(index));
+                			}
+                			break;
+                		
+//                case XMLEvent.START_ELEMENT:
+//                    int attributes = r.getAttributeCount();
+//                    for(int i=0;i<attributes;++i) {
+//                    }
+//                break;
+                	
+                	}
+                	break;
+            }
+            
+            i++;
+		}
+		
+		return "";
+	}
+
+	public void Writer() {
+	}
 }
